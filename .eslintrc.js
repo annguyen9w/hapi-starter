8ff63f4a59ed359cc3e260706188f4a28fb48639
an.nguyen <an.nguyen@9thwonder.com>
2021-12-02 11:53:19 +0700

    update eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     'airbnb-base',
-    'airbnb-typescript/base',
+    'airbnb-typescript/base'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
   plugins: [
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
+  ignorePatterns: ['.eslintrc.js', 'node_modules/', 'dist/'],
   rules: {
     '@typescript-eslint/comma-dangle': ['error', 'never'],
     '@typescript-eslint/semi': ['error', 'never'],
     'import/prefer-default-export': 'off',
     'no-restricted-syntax': 'off',
-    'linebreak-style': 'off',
-  },
-};
+    'linebreak-style': 'off'
+  }
+}
